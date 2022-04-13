6a5656d2d302ff2b3a26595cceef10b44754949c
Marty de Viterbo <mmdeviterbo@up.edu.ph>
2022-04-13 11:50:04 +0800

    update middleware

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,10 +1,19 @@
 import type { NextRequest } from 'next/server'
 import { NextResponse } from "next/server";
-import authenticate from '../src/utils/authenticate';
+import bcrypt from 'bcryptjs'
 
 export default async function middleware(req: NextRequest) {
   const url = req.nextUrl.clone()
-  const isAuth: boolean = await authenticate(req)
+  let isAuth: boolean = false
+
+  try{
+    const { currentUser } = req.cookies
+    const {email, full_name, token} = JSON.parse(currentUser)
+    const namesToken: string = email + '' + full_name + '19981012*'
+    isAuth = await bcrypt.compare(namesToken, token)
+  }catch(err){
+    isAuth = false
+  }
 
   //is there active/valid user?
   if(!isAuth) {
