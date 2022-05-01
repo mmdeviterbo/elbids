6de9956f9f5e47463f367560eae89d97e1eb68e0
Marty de Viterbo <mmdeviterbo@up.edu.ph>
2022-05-01 09:03:15 +0800

    9:03AM

--- src/utils/getURI.ts
+++ src/utils/getURI.ts
@@ -1,5 +1,5 @@
 const getURI=(path: string): string=>{
-  const prod = path   //'production'
+  const prod = "https://elbids.herokuapp.com"   //'production'
   const dev = `http://localhost:8080${path}`         //'development'
   return process.env.NODE_ENV ==='production'? prod : dev 
 }
